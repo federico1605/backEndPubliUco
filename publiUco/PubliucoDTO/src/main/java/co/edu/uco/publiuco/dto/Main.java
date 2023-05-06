5f130e0c136bdb78b890ec6339bff19024c8cb8f
Federico zapata <fede.zapata16@gmail.com>
2023-05-06 18:23:27 -0500

    Se crearon todos los proyectos y se arreglaron los conflictos

---
+++ publiUco/PubliucoDTO/src/main/java/co/edu/uco/publiuco/dto/Main.java
@@ -0,0 +1,7 @@
+package co.edu.uco.publiuco.dto;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ publiUco/UcoCrossCutting/src/main/java/co/edu/uco/crosscutting/Main.java
@@ -0,0 +1,7 @@
+package co.edu.uco.crosscutting;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
